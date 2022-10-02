2bdf3c1add8dfaf2a196c29b0760e08985b651e1
Rofl-delhite <ayushsaxena994@gmail.com>
2022-10-03 00:13:22 +0530

    homePage-and-navs

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.get("demo", (req, res) => {
+  res.sendFile(path.join(__dirname, "demo.html"));
+});
+app.get("doc", (req, res) => {
+  res.sendFile(path.join(__dirname, "doc.html"));
+});
 app.listen(process.env.PORT || port, () => {
   console.log("http://localhost:3000");
 });
